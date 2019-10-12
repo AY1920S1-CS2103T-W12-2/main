8b4748c5727dbdfe52789fafac1d6565978e99e3
krusagiz <acgmelvin@gmail.com>
2019-10-12 21:11:19 +0800

    Created UntagCommand Tests
    - Modified TagCommand tests

--- src/test/java/thrift/logic/commands/UntagCommandTest.java
+++ src/test/java/thrift/logic/commands/UntagCommandTest.java
@@ -21,21 +21,21 @@ import thrift.testutil.TagSetBuilder;
 import thrift.testutil.TypicalIndexes;
 import thrift.testutil.TypicalTransactions;
 
-class TagCommandTest {
+class UntagCommandTest {
 
 
     @Test
-    void execute_newTags_success() throws CommandException {
+    void execute_existTags_success() throws CommandException {
         Model model = new ModelManager(TypicalTransactions.getTypicalThrift(), new UserPrefs(),
                 new PastUndoableCommands());
         Expense originalExpense = new ExpenseBuilder(model.getFilteredTransactionList().get(0))
-                .build();
+                .withTags("Debt", "Horror", "Bliss").build();
+        model.setTransaction(model.getFilteredTransactionList().get(0), originalExpense);
 
-        Set<Tag> tagSet = new TagSetBuilder("Food", "Shopping").build();
-        TagCommand tagCommand = new TagCommand(TypicalIndexes.INDEX_FIRST_TRANSACTION, tagSet);
+        Set<Tag> tagSet = new TagSetBuilder("Debt", "Horror").build();
+        UntagCommand untagCommand = new UntagCommand(TypicalIndexes.INDEX_FIRST_TRANSACTION, tagSet);
 
-        Set<Tag> expectedTag = new TagSetBuilder(originalExpense.getTags())
-                .addTags("Food", "Shopping")
+        Set<Tag> expectedTag = new TagSetBuilder("Bliss")
                 .build();
         Expense updatedExpense = new Expense(
                 originalExpense.getDescription(),
@@ -45,7 +45,7 @@ class TagCommandTest {
                 expectedTag);
 
         assertNotEquals(updatedExpense.getTags(), model.getFilteredTransactionList().get(0).getTags()); //Before
-        tagCommand.execute(model);
+        untagCommand.execute(model);
         assertEquals(updatedExpense.getTags(), model.getFilteredTransactionList().get(0).getTags()); //After
     }
 
@@ -54,24 +54,24 @@ class TagCommandTest {
     void testEquals() {
         Set<Tag> setOne = new TagSetBuilder("Food", "Shopping").build();
         Set<Tag> setTwo = new TagSetBuilder("Debt", "Fees").build();
-        TagCommand firstTagCommand = new TagCommand(TypicalIndexes.INDEX_FIRST_TRANSACTION, setOne);
-        TagCommand secondTagCommand = new TagCommand(TypicalIndexes.INDEX_FIRST_TRANSACTION, setTwo);
+        UntagCommand firstUntagCommand = new UntagCommand(TypicalIndexes.INDEX_FIRST_TRANSACTION, setOne);
+        UntagCommand secondUntagCommand = new UntagCommand(TypicalIndexes.INDEX_FIRST_TRANSACTION, setTwo);
 
         // same object -> returns true
-        assertTrue(firstTagCommand.equals(firstTagCommand));
+        assertTrue(firstUntagCommand.equals(firstUntagCommand));
 
         // same values -> returns true
-        TagCommand firstTagCommandCopy = new TagCommand(TypicalIndexes.INDEX_FIRST_TRANSACTION, setOne);
-        assertTrue(firstTagCommand.equals(firstTagCommandCopy));
+        UntagCommand firstUntagCommandCopy = new UntagCommand(TypicalIndexes.INDEX_FIRST_TRANSACTION, setOne);
+        assertTrue(firstUntagCommand.equals(firstUntagCommandCopy));
 
         // different types -> returns false
-        assertFalse(firstTagCommand.equals(1));
+        assertFalse(firstUntagCommand.equals(1));
 
         // null -> returns false
-        assertFalse(firstTagCommand.equals(null));
+        assertFalse(firstUntagCommand.equals(null));
 
         // different transaction -> returns false
-        assertFalse(firstTagCommand.equals(secondTagCommand));
+        assertFalse(firstUntagCommand.equals(secondUntagCommand));
     }
 
 }
--- src/test/java/thrift/logic/parser/TagCommandParserTest.java
+++ src/test/java/thrift/logic/parser/TagCommandParserTest.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static thrift.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static thrift.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static thrift.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
+import thrift.commons.core.Messages;
 import thrift.logic.commands.TagCommand;
 import thrift.testutil.TagSetBuilder;
 import thrift.testutil.TypicalIndexes;
@@ -18,11 +21,13 @@ class TagCommandParserTest {
     @Test
     void parse_with_Tags() {
         String input = "tag i/1 t/Food t/Shopping";
+        String input2 = "tag i/1 t/Shopping t/Food" ;
 
         TagCommand expectedCommand = new TagCommand(TypicalIndexes.INDEX_FIRST_TRANSACTION,
                 new TagSetBuilder("Food", "Shopping").build());
 
         assertParseSuccess(parser, input, expectedCommand);
+        assertParseSuccess(parser, input2, expectedCommand);
     }
 
     @Test
@@ -30,5 +35,12 @@ class TagCommandParserTest {
         String input = "tag i/1 t/";
 
         assertParseFailure(parser, input, TagCommand.MESSAGE_NOT_TAGGED);
+    }
+
+    @Test
+    void parse_wrongSyntax() {
+        String input = "tag";
+
+        assertParseFailure(parser, input, String.format(MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE));
     }
 }
